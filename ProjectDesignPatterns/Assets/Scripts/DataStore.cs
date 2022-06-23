c8ced956d05b84d660aba3add5eea1bea69bc4c8
EdissonRivera <edisonriveraing@gmail.com>
2022-06-23 01:18:49 -0500

    Pattern  Adapter
    Patron adapter, escoger un comportamiento u otro
    
    (Joystick-Input)

--- ProjectDesignPatterns/Assets/Scripts/DataStore.cs
+++
@@ -1,9 +0,0 @@
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-public interface DataStore
-{
-    void SetData<T>(T data, string name);
-    T GetData<T>(string name);
-}
